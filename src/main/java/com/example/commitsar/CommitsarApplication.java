0682e9b23ddf98da67d1861762a74c2fa6a0ddc9
vlad raykovsky <vraykovsky2016@gmail.com>
2020-12-02 14:10:58 +0200

    BREAKING CHANGE: initial commit

---
+++ src/main/java/com/example/commitsar/CommitsarApplication.java
@@ -0,0 +1,13 @@
+package com.example.commitsar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommitsarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommitsarApplication.class, args);
+    }
+
+}
